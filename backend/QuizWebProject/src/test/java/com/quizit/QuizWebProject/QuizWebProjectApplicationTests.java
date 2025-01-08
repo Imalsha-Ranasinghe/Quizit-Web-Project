0a5d40add0e5486d49c01b77120fb98a16395a18
Imalsha-Ranasinghe <imalsharanasinghe2020@gmail.com>
2025-01-08 10:21:05 +0530

    initial

---
+++ backend/QuizWebProject/src/test/java/com/quizit/QuizWebProject/QuizWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizit.QuizWebProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
