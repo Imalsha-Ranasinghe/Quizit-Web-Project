e7d5f4691bf9877b93f8033aa140343bb9bb4a35
NinaMadu <ninadamaduwantha@gmail.com>
2025-02-25 10:52:01 +0530

    backend initilal

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db.js");
+
+const authRoutes = require("./routes/authRoutes.js");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Quiz API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
